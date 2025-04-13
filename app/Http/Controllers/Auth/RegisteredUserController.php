2a2d269683a7922cc7dcebcd6b9fee8716db49ae
VincentNdegwa <ndegwavincent7@gmail.com>
2025-04-13 22:20:36 +0300

    Add user_type field to User model; implement isSuperAdmin method and update redirection logic in AuthenticatedSessionController

--- app/Http/Controllers/Auth/RegisteredUserController.php
+++ app/Http/Controllers/Auth/RegisteredUserController.php
@@ -40,6 +40,7 @@ class RegisteredUserController extends Controller
             'name' => $request->name,
             'email' => $request->email,
             'password' => Hash::make($request->password),
+            'user_type' => 'Admin'
         ]);
 
         event(new Registered($user));
