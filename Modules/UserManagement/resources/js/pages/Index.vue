27609085dc23c47cb5f8ed7bef77409b02de6493
VincentNdegwa <ndegwavincent7@gmail.com>
2025-04-21 08:52:07 +0300

    feat: Update Index.vue and Index.stub to include breadcrumbs for improved navigation

--- Modules/UserManagement/resources/js/pages/Index.vue
+++ Modules/UserManagement/resources/js/pages/Index.vue
@@ -1,22 +1,27 @@
 <script setup lang="ts">
 import { Head } from '@inertiajs/vue3';
 import AppLayout from '@/Layouts/AppLayout.vue';
+import { type BreadcrumbItem } from '@/types';
+const breadcrumbs: BreadcrumbItem[] = [
+    {
+        title: 'Dashboard',
+        href: 'dashboard',
+    },
+    {
+        title: 'UserManagement',
+        href: 'usermanagement.index',
+    },
+];
 
 </script>
 
 <template>
   <Head title="UserManagement" />
   
-  <AppLayout>
-    <template #header>
-      <h2 class="font-semibold text-xl text-gray-800 dark:text-gray-200 leading-tight">
-        UserManagement
-      </h2>
-    </template>
-
+  <AppLayout :breadcrumbs="breadcrumbs" >
     <div class="py-12">
       <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
-        <div class="bg-white dark:bg-gray-800 overflow-hidden shadow-xl sm:rounded-lg p-6">
+        <div class="overflow-hidden shadow-xl sm:rounded-lg p-6">
           <h1 class="text-2xl font-bold mb-4">Welcome to the UserManagement Module</h1>
           <p>This is the index page of your new module.</p>
         </div>
