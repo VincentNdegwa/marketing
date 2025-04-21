31d006b0e5fd4c89ab70f265e7fe9485161ff04c
VincentNdegwa <ndegwavincent7@gmail.com>
2025-04-21 21:38:34 +0300

    fix: Correct namespace import for Business model in LaratrustSeeder

--- database/seeders/LaratrustSeeder.php
+++ database/seeders/LaratrustSeeder.php
@@ -2,11 +2,11 @@
 
 namespace Database\Seeders;
 
-use App\Models\Business;
 use App\Models\Role;
 use App\Models\User;
 use Illuminate\Database\Seeder;
 use Illuminate\Support\Facades\Hash;
+use Modules\Business\app\Models\Business;
 
 class LaratrustSeeder extends Seeder
 {
