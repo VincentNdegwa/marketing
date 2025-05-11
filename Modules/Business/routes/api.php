8f7bc3f341e439f61a4edf8d4934c0f8f8b5a083
Vincent Ndegwa Ndungu <116282941+VincentNdegwa@users.noreply.github.com>
2025-05-11 15:55:19 +0300

    Delete Modules directory

--- Modules/Business/routes/api.php
+++
@@ -1,8 +0,0 @@
-<?php
-
-use Illuminate\Support\Facades\Route;
-use Modules\Business\App\Http\Controllers\BusinessController;
-
-Route::middleware(['auth:sanctum'])->prefix('v1')->group(function () {
-    Route::apiResource('business', BusinessController::class)->names('business');
-});
--- Modules/UserManagement/routes/api.php
+++
@@ -1,8 +0,0 @@
-<?php
-
-use Illuminate\Support\Facades\Route;
-use Modules\UserManagement\App\Http\Controllers\UserManagementController;
-
-Route::middleware(['auth:sanctum'])->prefix('v1')->group(function () {
-    Route::apiResource('usermanagement', UserManagementController::class)->names('usermanagement');
-});
